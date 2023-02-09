ebf6f15d4ad9c1b76d2c239fd8b14f4a1118ea1a
MaionePietro <pietromaionespam@gmail.com>
2023-02-09 16:17:48 +0100

    FEAT cont cella

--- js/app.js
+++ js/app.js
@@ -1,10 +1,18 @@
+let latoGriglia = 10
+let numeroCelle = latoGriglia * latoGriglia
 
 const preGame = document.querySelector('section');
 const inGame = document.querySelector('section:last-child')
-
+const grigliaElement = document.querySelector('.griglia')
 const btnElement = document.querySelector('.btn');
 
 btnElement.addEventListener('click', function (){
     preGame.classList.remove('active');
     inGame.classList.add('active');
 })
+
+for (let i = 0; i < numeroCelle; i++) {
+    let num = i + 1;
+    let divString = `<div class="cella" style="width: calc(100% / ${latoGriglia});">${num}</div>`;
+    grigliaElement.innerHTML += divString
+}
